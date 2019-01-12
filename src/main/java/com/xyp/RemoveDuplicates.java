be7df571337dbdf3b1588b58beb78490900f8110
Samuel Xue <21xyphacker@gmail.com>
2019-01-12 13:41:44 +0800

    removeDuplicates

---
+++ src/main/java/com/xyp/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+package com.xyp;
+
+public class RemoveDuplicates {
+
+	public static int removeDuplicates(int[] nums) {
+
+		int nowNum = nums[0];
+
+		int nowIndex = 1;
+
+		for (int i = 1; i < nums.length; i++) {
+			if (nowNum != nums[i]) {
+				nowNum = nums[i];
+				nums[nowIndex] = nums[i];
+				nowIndex++;
+			}
+
+		}
+
+		return nowIndex;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 4,4,4,4,4 };
+		int length = removeDuplicates(nums);
+		System.out.println("asd");
+	}
+
+}
