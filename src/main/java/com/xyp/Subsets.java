01e5909d445b44f59bde21274b7178547b25b8d5
21xyp <21xyphacker@gmail.com>
2019-01-30 11:06:54 +0800

    fix bug

--- src/main/java/com/xyp/Subsets.java
+++ src/main/java/com/xyp/Subsets.java
@@ -4,13 +4,33 @@ import java.util.*;
 
 public class Subsets {
 
+
+    static int sockMerchant(int n, int[] ar) {
+        Map<Integer, Integer> numberMap = new HashMap<>();
+        for (int number : ar) {
+            if (numberMap.get(number) != null) {
+                int count = numberMap.get(number);
+                count = count + 1;
+                numberMap.put(number, count);
+            } else {
+                numberMap.put(number, 1);
+            }
+        }
+
+        int sum = 0;
+        for (int count : numberMap.values()) {
+            sum = sum + count / 2;
+        }
+        return sum;
+    }
+
     public static List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> subsets = new ArrayList<>();
 
         for (int index = 0; index < 2 << nums.length - 1; index++) {
             List<Integer> subSet = new ArrayList<>();
             String subString = Integer.toBinaryString(index);
-            String newSubString=subString;
+            String newSubString = subString;
             if (subString.length() < nums.length) {
                 for (int i = 0; i < nums.length - subString.length(); i++) {
                     newSubString = "0" + newSubString;
